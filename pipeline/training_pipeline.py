b61b696a5ab45c8678987db3db8a959fa8cfb608
nishant129 <nishantborkar139@gmail.com>
2025-06-25 11:58:05 +0530

    add dvc

---
+++ pipeline/training_pipeline.py
@@ -0,0 +1,12 @@
+from src.data_processing import DataProcessor
+from utils.common_functions import read_yaml
+from src.model_training import ModelTraining
+from config.paths_config import *
+
+if __name__ == "__main__":
+
+    data_processor =  DataProcessor(ANIMELIST_CSV,PROCESSED_DIR)
+    data_processor.run()
+    
+    model_trainer = ModelTraining(PROCESSED_DIR)
+    model_trainer.train_model()
